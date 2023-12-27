ee6c11f62972792018fd17fded4595ce15d21e7d
Pallavi <pan585@usask.ca>
2023-12-26 21:40:37 -0600

    Created dict to store data

--- index.py
+++ index.py
@@ -2,5 +2,14 @@
 import pandas as pd
 df = pd.read_excel(r"data.xlsx", index_col=None, na_values=['NA'], usecols = "A")
 
+data = df.value_counts()
+print(data)
 
+# creating dictionary of dictionary
+dict = {}
 
+for i in range(50):
+    dict[i] = 0
+    
+
+print(dict)
